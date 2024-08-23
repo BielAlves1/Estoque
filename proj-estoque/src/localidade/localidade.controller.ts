a5a27eba5a87c6d2a7f90622f03bc70feee7f847
BielAlves1 <gabriel2002.alves1010@gmail.com>
2024-08-23 18:03:39 -0300

    Ajuste

--- proj-estoque/src/localidade/localidade.controller.ts
+++ proj-estoque/src/localidade/localidade.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, HttpStatus } from '@nestjs/common';
 import { LocalidadeService } from './localidade.service';
 import { CreateLocalidadeDto } from './dto/create-localidade.dto';
-import { UpdateLocalidadeDto } from './dto/update-localidade.dto';
+import { Response } from 'express';
 
 @Controller('localidade')
 export class LocalidadeController {
   constructor(private readonly localidadeService: LocalidadeService) {}
 
   @Post('read-create')
-  findOrCreate(@Body() createLocalidadeDto: CreateLocalidadeDto) {
-    return this.localidadeService.findOrCreate(createLocalidadeDto);
+  findOrCreate(@Body() createLocalidadeDto: CreateLocalidadeDto, @Res() res: Response) {
+    const result = this.localidadeService.findOrCreate(createLocalidadeDto);
+    if (result.statusCode === HttpStatus.OK) {
+      return res.status(HttpStatus.OK).json(result);
+    } else if (result.statusCode === HttpStatus.CREATED) {
+      return res.status(HttpStatus.CREATED).json(result);
+    } 
   }
 
   @Get('read-all')
--- proj-estoque/src/localidade/localidade.service.ts
+++ proj-estoque/src/localidade/localidade.service.ts
@@ -1,36 +1,49 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateLocalidadeDto } from './dto/create-localidade.dto';
 import { PrismaService } from 'src/database/prisma.service';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import { Localidade } from './entities/localidade.entity';
+import { Response } from 'express';
 
 @Injectable()
 export class LocalidadeService {
   constructor(private readonly prisma: PrismaService, private readonly httpService: HttpService) {}
 
-  async findOrCreate(createLocalidadeDto: CreateLocalidadeDto) {
+  async findOrCreate(createLocalidadeDto: CreateLocalidadeDto, res: Response) {
     const { municipio_nome } = createLocalidadeDto;
 
-    const existingLocalidade = await this.prisma.localidade.findFirst({
-      where: {
-        municipio_nome
-      },
-    });
+    // const existingLocalidade = await this.prisma.localidade.findFirst({
+    //   where: {
+    //     municipio_nome,
+    //   },
+    // });
+    
+    const existingLocalidade = await this.findCity(municipio_nome)
 
     if (existingLocalidade) {
-      return existingLocalidade;
+      return res.status(HttpStatus.OK).json({
+        statusCode: HttpStatus.OK,
+        message: 'Localidade já existe',
+        data: existingLocalidade,
+      });
     }
 
     const municiNomeFormat = municipio_nome.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/ç/g, 'c').replace(/ /g, '-'); 
 
 
+  let localidadeData;
+  try {
     const ibgeURL = `https://servicodados.ibge.gov.br/api/v1/localidades/municipios/${municiNomeFormat}`;
     const response = await firstValueFrom(this.httpService.get(ibgeURL));
+    localidadeData = response.data;
+  } catch (error) {
+    throw new HttpException(`Erro ao acessar a API do IBGE: ${error.message}`, HttpStatus.BAD_REQUEST);
+  }
 
-    const localidadeData = response.data;
-    if (!localidadeData) {
-      throw new NotFoundException(`Município ${municipio_nome} não encontrado na API do IBGE`);
-    }
+  if (!localidadeData || !localidadeData.id) {
+    throw new NotFoundException(`Município ${municipio_nome} não encontrado na API do IBGE`);
+  }
 
     const newLocalidade = {
       municipio_id: localidadeData.id,
@@ -40,9 +53,15 @@ export class LocalidadeService {
       uf_nome: localidadeData.microrregiao.mesorregiao.UF.nome,
     };
 
-    return await this.prisma.localidade.create({
+    const createdLocalidade = await this.prisma.localidade.create({
       data: newLocalidade,
     });
+  
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: 'Localidade criada com sucesso',
+      data: createdLocalidade,
+    };
   }
 
   async findAll() {
@@ -55,7 +74,7 @@ export class LocalidadeService {
     }
   }
 
-  async findCity(nome: string) {
+  async findCity(nome: string): Promise<Localidade[]> {
     const localidades = await this.prisma.localidade.findMany({
       where: {
         municipio_nome: {
@@ -64,7 +83,11 @@ export class LocalidadeService {
         },
       },
     });
-
+  
+    if (localidades.length === 0) {
+      throw new NotFoundException('Não existem cidades cadastradas para listar.');
+    }
+  
     return localidades;
   }
 }
